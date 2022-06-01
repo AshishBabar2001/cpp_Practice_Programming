0c698f41b7b7b581e53b8e3e7a70937522da2624
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-06-01 12:46:26 +0530

    Add files via upload

---
+++ 02_Addition_of_Two_Numbers.cpp
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+
+    printf("Enter Two Number : ");
+    scanf("%d %d",&a,&b);
+
+    c = a + b;
+
+    printf("%d",c);
+    return 0;
+}
