c300f745df3af61b08d0a0f70582d0f599c3e26a
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-06-02 18:18:08 +0530

    Add files via upload

---
+++ 04_Type_Modifiers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    short int aa;
+    long int bb;
+
+    cout<<"Size of Short Int : "<<sizeof(aa)<<endl;
+    cout<<"size of LOng Int : "<<sizeof(bb)<<endl;
+
+}
