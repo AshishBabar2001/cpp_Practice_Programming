a55258a00629c8359c3c51b2556561d61f4c31b5
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-06-03 11:04:27 +0530

    Add files via upload

---
+++ 08_Elseif_Condition.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int Savings;
+    cin>>Savings;
+
+    if(Savings>5000)
+    {
+        cout<<"Neha"<<endl;
+    }
+    else if(Savings>2000)
+    {
+        cout<<"Shruti"<<endl;
+    }
+    else
+    {
+        cout<<"Friends"<<endl;
+    }
+}
