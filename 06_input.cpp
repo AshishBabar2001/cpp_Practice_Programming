c3cf4824f37a650844591c52a47c901ad58de2f3
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-06-03 11:04:11 +0530

    Add files via upload

---
+++ 06_input.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int Amount1;
+    cin>>Amount1;
+
+    int Amount2;
+    cin>>Amount2;
+
+    int sum = Amount1+Amount2;
+
+    cout<<sum<<endl;
+
+}
