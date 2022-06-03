4fc03cb5e0fa0472d9631629391329b2fbf11df4
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-06-03 11:04:20 +0530

    Add files via upload

---
+++ 07_If_Else.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int Savings;
+    cin>>Savings;
+
+    if(Savings>5000)
+    {
+        cout<<"Neha"<<endl;
+    }
+    else
+    {
+        cout<<"Shruti";
+    }
+    
+}
