111a4eefc06feda6bbb14681b315457ea2f6fdf7
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-06-07 23:32:20 +0530

    Add files via upload

---
+++ 11_odd_or_even.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    if(n%2==0)
+    {
+        cout<<n<<" Number Is Even"<<endl;
+    }
+    else
+    {
+        cout<<n<<" Number Is Odd"<<endl;
+    }
+}
